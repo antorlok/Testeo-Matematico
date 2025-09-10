10f784a086387a13a8e39522d625d6c046217009
Abisaac1809 <abisaaccarmona@gmail.com>
2025-09-09 21:52:25 -0400

    feat: se agrega wkhtmltopdf al programa y se actualizan las dependencias del requirements.txt

--- Apps/Documentation/pdf_generator.py
+++ Apps/Documentation/pdf_generator.py
@@ -45,11 +45,24 @@ def generate_pdf():
     options = {
         'enable-local-file-access': None  # Permite acceso a archivos locales para imágenes
     }
-    wkhtmltopdf_path = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
+    # Buscar wkhtmltopdf primero en una ruta local, luego en variable de entorno, luego usar default
+    local_wkhtmltopdf = os.path.join(os.path.dirname(__file__), 'wkhtmltopdf', 'bin' ,'wkhtmltopdf.exe')
+    default_wkhtmltopdf = r'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
+
+    if os.path.isfile(local_wkhtmltopdf):
+        wkhtmltopdf_path = local_wkhtmltopdf
+    elif os.path.isfile(default_wkhtmltopdf):
+        wkhtmltopdf_path = default_wkhtmltopdf
+    else:
+        raise FileNotFoundError(
+            'No se encontró el ejecutable wkhtmltopdf.\n'
+            'Coloca wkhtmltopdf.exe en "Apps/Documentation/wkhtmltopdf/", o define la variable de entorno WKHTMLTOPDF_PATH, '
+            'o instala wkhtmltopdf en "C:/Program Files/wkhtmltopdf/bin/".'
+        )
     config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
 
     # Generar PDF a partir del HTML
-    pdf_output_path = os.path.join(os.path.dirname(__file__), "documentation.pdf")
+    pdf_output_path = os.path.join(os.path.dirname(__file__), "documentation1.pdf")
     pdfkit.from_file(temp_html_path, pdf_output_path, options=options, configuration=config)
 
     # Eliminar archivo HTML temporal
